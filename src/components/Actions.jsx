cbfffa21787c2ba500096bc6fba2d837687f2c12
Gerard Braad <me@gbraad.nl>
2021-12-10 15:06:38 +0800

    Initial commit

---
+++ src/components/Actions.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+    Button,
+} from '@patternfly/react-core';
+
+export default class Actions extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <Button onClick={this.props.onStartClicked}
+                    variant="primary">Start</Button>{' '}
+                <Button onClick={this.props.onStopClicked}
+                    variant="secondary">Stop</Button>{' '}
+                <Button onClick={this.props.onDeleteClicked}
+                    variant="danger">Delete</Button>
+            </div>
+        );
+    }
+}
+
+Actions.propTypes = {
+    onStartClicked: PropTypes.func,
+    onStopClicked: PropTypes.func,
+    onDeleteClicked: PropTypes.func
+};
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Actions from './components/Actions.jsx';
+import LogWindow from './components/LogWindow.jsx';
+import Settings from './components/Settings.jsx';
+import Status from './components/Status.jsx';
+
+export {
+  Actions,
+  LogWindow,
+  Settings,
+  Status
+};
