39b81ee16ec8099644d0e4d53dcb324c8de3fd85
Gerard Braad <me@gbraad.nl>
2021-12-20 13:22:56 +0800

    Rename Settings -> Configuration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import Actions from './components/Actions.jsx';
 import ControlCard from './components/ControlCard.jsx';
 import LogWindow from './components/LogWindow.jsx';
-import Settings from './components/Settings.jsx';
+import Configuration from './components/Configuration.jsx';
 import Status from './components/Status.jsx';
 
 export {
   Actions,
   ControlCard,
   LogWindow,
-  Settings,
+  Configuration,
   Status
 };
